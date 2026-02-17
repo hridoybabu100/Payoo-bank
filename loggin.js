bacb832c692f8d9a93f4a18eacfb91fff920a9bd
Hridoy Hasan Babu <hridoyakanda100@gmail.com>
2026-02-18 00:41:31 +0600

    new updated

---
+++ loggin.js
@@ -0,0 +1,26 @@
+// console.log('just java logging'); 
+
+const logginButton = document.getElementById('loggin-btn').addEventListener('click', function(){
+    
+    const text = document.getElementById('input-text');
+    const contactText = text.value ;
+    // console.log(contactText); 
+    
+    
+    const input = document.getElementById('input-pass');
+    const InputPass = input.value ;
+    // console.log(InputPass); 
+
+    if ( contactText === '01234567890' && InputPass === '1234'){
+        alert('Successfully logged In');
+        window.location.assign("/home.html")
+        
+    }
+    else{
+        alert('Loggin failed');
+        return;
+    }
+    
+    
+    
+})
